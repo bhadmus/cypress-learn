e7adf9658f0d6965ae88748474b6b6e8d61083c6
Ademola Bhadmus <bhadmusademola.1@gmail.com>
2022-01-10 12:15:46 +0000

    code readability implemented

--- cypress/integration/test/index.spec.js
+++ cypress/integration/test/index.spec.js
@@ -1,14 +1,17 @@
 import PageObjects from '../../support/pageObjects/pageObjects'
 const actions = new PageObjects()
+
 describe('Test quidax login', ()=>{
     it('Launch the Site', ()=>{
         cy.visit('/');
     })
+
     it('Click the sign in icon', ()=>{
         cy.fixture('elementMapper').then((em)=>{
             actions.clickAnElement(em.signInIcon);
         })
     })
+
     it('Insert Credentials', ()=>{
         cy.fixture('elementMapper').then((em)=>{
             actions.typeAText(em.emailField, em.emailText);
@@ -16,12 +19,14 @@ describe('Test quidax login', ()=>{
 
         })
     })
+
     it('Click the sign in button', ()=>{
         cy.fixture('elementMapper').then((em)=>{
             actions.clickAnElement(em.signInButton);
 
         })
     })
+    
     it('Verify the presence of Verify Button', ()=>{
         cy.fixture('elementMapper').then((em)=>{
             actions.pageLoader(em.verifyButton);
