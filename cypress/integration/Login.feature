24ba97f26fa71a4eb2ef52dc924a038dae5aa88a
Ademola Bhadmus <bhadmusademola.1@gmail.com>
2022-01-10 12:07:33 +0000

    implemented BDD

--- cypress/integration/Login.feature
+++ cypress/integration/Login.feature
@@ -5,8 +5,8 @@ Feature: Quidax App
 
     Scenario: A user should be able to login with valid details
         Given I am on the quidax site
-        When I click the sign in icon
-        And I insert the username
-        And I insert the password
-        And I click the sign in button
+        When I click the "sign in icon"
+        And I insert the "username"
+        And I insert the "password"
+        And I click the "sign in button"
         Then I should see the authentication page
--- cypress/integration/Login/steps.spec.js
+++ cypress/integration/Login/steps.spec.js
@@ -1,20 +1,33 @@
 Given('I am on the quidax site', ()=>{
     cy.openSite();
 })
-When('I click the sign in icon', ()=>{
+When('I click the {string}', (button)=>{
     cy.fixture('elementMapper').then((em)=>{
-        cy.clickAnElement();
+        switch(button){
+            case "sign in icon":
+                cy.clickAnElement(em.signInIcon);
+                break
+            case "sign in button":
+                cy.clickAnElement(em.signInButton);
+        }
     })
 })
-And('I insert the username', ()=>{
-    
-})
-And('I insert the password', ()=>{
-    
-})
-And('I click the sign in button', ()=>{
+And('I insert the {string}', (typeText)=>{
+    cy.fixture('elementMapper').then((em)=>{
+        switch(typeText){
+            case "username":
+                cy.typeSomeText(em.emailField, em.emailText);
+                break
+            case "password":
+                cy.typeSomeText(em.passwordField, em.passwordText);
+
+        }
+    })
     
 })
 Then('I should see the authentication page', ()=>{
+    cy.fixture('elementMapper').then((em)=>{
+        cy.verifyPresence(em.verifyButton);
+    })
     
 })
